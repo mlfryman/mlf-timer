da74608550139037864bb4056f99ce39e7433d46
Melanie L. Fryman <melanie@frymanet.com>
2014-10-15 15:39:23 -0500

    added phone vibration plugin

--- www/js/MainCtrl.js
+++ www/js/MainCtrl.js
@@ -7,12 +7,12 @@
 
     $scope.timerRunning = false;
 
-    $scope.startTimer = function (){
+    $scope.startTimer = function(){
       $scope.$broadcast('timer-start');
       $scope.timerRunning = true;
     };
 
-    $scope.stopTimer = function (){
+    $scope.stopTimer = function(){
       $scope.$broadcast('timer-stop');
       $scope.timerRunning = false;
       };
