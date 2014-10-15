0be5d4f758c61669b2a511a15f95bf76b1cc14a8
Melanie L. Fryman <melanie@frymanet.com>
2014-10-15 15:12:50 -0500

    input form

--- www/js/MainCtrl.js
+++ www/js/MainCtrl.js
@@ -1,9 +1,21 @@
 (function(){
   'use strict';
 
-  angular.module('mlf-timer', ['ionic'])
+  angular.module('mlf-timer', ['ionic', 'timer'])
   .controller('MainCtrl', ['$scope', function($scope){
     $scope.title = 'Timer';
+
+    $scope.timerRunning = false;
+
+    $scope.startTimer = function (){
+      $scope.$broadcast('timer-start');
+      $scope.timerRunning = true;
+    };
+
+    $scope.stopTimer = function (){
+      $scope.$broadcast('timer-stop');
+      $scope.timerRunning = false;
+      };
 
   }]);
 })();
