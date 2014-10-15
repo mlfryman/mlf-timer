82455924b08b2d293a561aec9a11033537b94cf6
Melanie L. Fryman <melanie@frymanet.com>
2014-10-15 16:06:39 -0500

    timer is working, needs styling

--- www/js/MainCtrl.js
+++ www/js/MainCtrl.js
@@ -3,7 +3,22 @@
 
   angular.module('mlf-timer', ['ionic', 'timer'])
   .controller('MainCtrl', ['$scope', function($scope){
-    $scope.title = 'Timer';
+    $scope.ms = $scope.hCvt + $scope.mCvt + $scope.sCvt;
+    $scope.h = '';
+    $scope.m = '';
+    $scope.s = '';
+
+    $scope.hCvt = function(){
+      return $scope.h * 3600000;
+    };
+    
+    $scope.mCvt = function(){
+      return $scope.m * 60000;
+    };
+
+    $scope.sCvt = function(){
+      return $scope.s * 1000;
+    };
 
     $scope.timerRunning = false;
 
